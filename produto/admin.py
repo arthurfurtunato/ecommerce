af25e261b9a1b20a6f24e2fc1b262a7c77f99781
arthur.furtunato <arthur.furtunato@lais.huol.ufrn.br>
2022-01-30 21:13:33 -0300

    Criação dos modelos Variacao e Produto, e atualização do admin

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from produto.models import Produto, Variacao
+from .models import Variacao
 
 # Register your models here.
+class VariacaoInline(admin.TabularInline):
+    model = Variacao
+    extra = 1
+
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [
+        VariacaoInline
+    ]
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Variacao)

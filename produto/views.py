533127f6c63b326fb65e3d3f02d9df65a520e31f
arthur.furtunato <arthur.furtunato@lais.huol.ufrn.br>
2022-02-21 13:42:38 -0300

    view add carrinho parte 1

--- produto/views.py
+++ produto/views.py
@@ -1,6 +1,6 @@
-from email import message
+from django.contrib import messages
 from django.urls import reverse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
@@ -29,13 +29,28 @@ class AdicionarAoCarrinho(View):
         variacao_id = self.request.GET.get('vid')
 
         if not variacao_id:
-            message.error(
+            messages.error(
                 self.request,
                 'Produto não existe'
             )
             return redirect(http_referer)
 
-        return HttpResponse('Adicionar Carrinho')
+        variacao = get_object_or_404(Variacao, id=variacao_id)
+        
+        if not self.request.session.get('carrinho'):
+            self.request.session['carrinho'] = {}
+            self.request.session.save()
+
+        carrinho = self.request.session['carrinho']
+
+        if variacao_id in carrinho:
+            # TODO: Variação existe no carrinho
+            pass
+        else:
+            # TODO: Variação não existe no carrinho
+            pass
+
+        return HttpResponse(f'Produto: {variacao.produto} <br> Variação: {variacao.nome}')
 
 class RemoverDoCarrinho(View):
     def get(self, *args, **kwargs):
